3862dcc5df12b532b8c66ddbb08d6f035343d8b1
WilliamFischer <williamfischer363@gmail.com>
2020-09-02 17:07:57 +1000

    + reset password

--- src/app/forgot-pass/forgot-pass.component.ts
+++ src/app/forgot-pass/forgot-pass.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Firebase
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-forgot-pass',
   templateUrl: './forgot-pass.component.html',
-  styleUrls: ['./forgot-pass.component.css']
+  styleUrls: ['./forgot-pass.component.scss']
 })
 export class ForgotPassComponent implements OnInit {
 
-  constructor() { }
+  userEmail : string;
+
+  constructor(
+    private router: Router,
+    private fireStore : AngularFirestore,
+    private afAuth : AngularFireAuth
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  resetPassword() {
+    if(this.userEmail){
+      return this.afAuth.sendPasswordResetEmail(this.userEmail)
+        .then(() => alert('Sent Password Reset Email!'))
+        .catch((error) => console.log(error))
+    }else{
+      alert('Please enter an email address')
+    }
   }
 
 }
